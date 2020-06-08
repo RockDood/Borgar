37a8b0d692aea6262a852d4a8d9a2d9bf2d7a2eb
Nathan <nleacroy@gmail.com>
2020-06-08 16:43:04 +0900

    House Layout
    house machine go brrt

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
+using Mirror;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
 
     public CharacterController controller;
